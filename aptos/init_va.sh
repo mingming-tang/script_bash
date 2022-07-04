e0075b2d0ae4a3de296e77b0d2d613444031f9bb
mingming.tang <mingmingtang@aliyun.com>
2022-07-04 16:46:24 +0800

    add init_va.sh

---
+++ aptos/init_va.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+./aptos genesis generate-keys --output-dir /opt/aptos_testnet
+
+./aptos genesis set-validator-configuration \
+    --keys-dir /opt/aptos_testnet --local-repository-dir /opt/aptos_testnet \
+    --username $USERNAME \
+    --validator-host $HOST:6180 \
+    --full-node-host $HOST:6182
+
+tee /opt/aptos_testnet/layout.yaml <<-EOF
+---
+root_key: "F22409A93D1CD12D2FC92B5F8EB84CDCD24C348E32B3E7A720F3D2E288E63394"
+users:
+  - "$USERNAME"
+chain_id: 40
+min_stake: 0
+max_stake: 100000
+min_lockup_duration_secs: 0
+max_lockup_duration_secs: 2592000
+epoch_duration_secs: 86400
+initial_lockup_timestamp: 1656615600
+min_price_per_gas_unit: 1
+allow_new_validators: true
+EOF
+
+./aptos genesis generate-genesis --local-repository-dir /opt/aptos_testnet --output-dir /opt/aptos_testnet
